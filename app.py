cbd9ee6ada357513d35f4b59ac5cc4ec9a564d7b
decent <danielekwuazi@gmail.com>
2018-06-07 11:37:29 +0700

    init db

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config.from_pyfile('config.cfg')
+
+db = SQLAlchemy(app)
+
+class Test(db.Model):
+    id = db.Colmn(db.Integer, primary_key=True)
+
+    
+if __name__ == '__main__':
+    app.run()
