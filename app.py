c8b6edcad990e2430331efc7a86ec548d6276df5
decent <danielekwuazi@gmail.com>
2018-06-12 11:50:51 +0700

    extract weather imformation from  openweathermap api response

--- app.py
+++ app.py
@@ -7,4 +7,18 @@ app.config.from_pyfile('config.cfg')
 
 @app.route('/')
 def index():
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=d29e532297d901144394a2faf99e31ca'
+    city = 'Bangkok'
+    
+    
+    r = requests.get(url.format(city)).json()
+
+    weather = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'humidity': r['main']['humidity'],
+        'icon': r['weather'][0]['icon']
+    }
+    print(weather)
     return render_template('weather.html')
